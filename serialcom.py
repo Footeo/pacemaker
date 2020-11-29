91caba4977a077694c4c20c40fd0e41b76678b53
Footeo <footeo@mcmaster.ca>
2020-11-28 19:13:22 -0500

    serial fuckery

--- serialcom.py
+++ serialcom.py
@@ -19,7 +19,7 @@ def sendparameters(params,switch): #params is a list with the parameters that ne
         9: ['VVIR',12],
         10: ['DOOR',12]
     }
-    mode_sel = mode.get(switch)
+    # mode_sel = mode.get(switch)
     # cur_mode = mode_sel[0] #use current mode later when transmitting
 
     print("Typecasted parameters",params)
@@ -28,39 +28,42 @@ def sendparameters(params,switch): #params is a list with the parameters that ne
     time.sleep(1)
     print("Serial Port Info:", settings.ser) 
 
-    buff = ctypes.create_string_buffer(69)  
+    buff = ctypes.create_string_buffer(100)  
 
     ## AOO VOO
 
     #send = [['switch',switch],['LRL',0],['URL',0],['MSR',0],['FAVD',0],['AtrAmp',0],['VentAmp',0],['AtrPW',0],['VentPW',0],['AtrSense',0],['VentSense',0],['VRP',0],['ARP',0],['PVARP',0],['RateSmooth',0],['ActivityThresh',0],['ReactTime',0],["RespFact",0],['RecoveryTime',0]] 
 
-    struct.pack_into('<BBBHHHHddddHHHHHHHdHHH',buff,0,22,85,params[0][1],params[1][1],params[2][1],params[3][1],params[4][1],params[5][1],params[6][1],params[7][1],params[8][1],params[9][1],params[10][1],params[11][1],params[12][1],params[13][1],params[14][1],params[15][1],params[16][1],params[17][1],params[18][1],params[19][1])# < means Little Endian - uint8,uint8,uint16,uint16,double
+    struct.pack_into('<BBBHHHHddddHHHHHHdHHH',buff,0,22,85,params[0][1],params[1][1],params[2][1],params[3][1],params[4][1],params[5][1],params[6][1],params[7][1],params[8][1],params[9][1],params[10][1],params[11][1],params[12][1],params[13][1],params[14][1],params[15][1],params[16][1],params[17][1],params[18][1])# < means Little Endian - uint8,uint8,uint16,uint16,double
                                                     # parameter 3. can be either 34 -> echo, or 85 -> set. Which is 0x22 and 0x55 in hex respectively.
     print(buff)
     vals1 = settings.ser.write(buff)  #Write the bytes to the global serial port variable
     print("size of the package we are sending: ", vals1)
-    print("calcsize",struct.calcsize("<BBBHHHHddddHHHHHHHdHHH"))
+    print("calcsize",struct.calcsize("<BBBHHHHddddHHHHHHdHHH"))
 
     time.sleep(1) #1 sec delay
 
     count=0
 
-    buff = ctypes.create_string_buffer(85)  #new buffer  
+    buff = ctypes.create_string_buffer(87)  #new buffer  
 
     while True:  ## Should turn this infinite loop into a function call that will display the Egram
-
+        
+        # struct.pack_into('<BB',buff,0,22,34)
+        # settings.ser.write(buff)
         #When reading here we have to read the entire serial packet and read off the last 2 doubles which are atr and vent pacing signals.
 
         #Vent signal is first 
 
-        vals2 = settings.ser.read(85) #read's argument is the size of the package we are sending/recieving
+        vals2 = settings.ser.read(87) #read's argument is the size of the package we are sending/recieving
         print("reading... ", vals2)  
         
+        print(vals2[0])
 
         #How are we going to unpack the two pacing pins? I think we have to unpack everything and then only use the last 2 values for the egram?
-        print('unpack buff (in decimal): ', struct.unpack('<BBBHHHHddHHHHHHHHHHHHHHHH',buff))
+        # print('unpack buff (in decimal): ', struct.unpack('<BBBHHHHddddHHHHHHHdHHHdd',buff))
         count += 1
-        if count==5:
+        if count==3:
             break
 
     settings.ser.close()
--- test2.py
+++ test2.py
@@ -13,32 +13,41 @@ ser.open()
 time.sleep(1)
 print("Serial Port Info:", ser) 
 
-buff = ctypes.create_string_buffer(14)  
-
-struct.pack_into('<BBHHd',buff,0,22,85,120,10,4)# < means Little Endian - uint8,uint8,uint16,uint16,double
-                                                # parameter 3. can be either 34 -> echo, or 85 -> set. Which is 0x22 and 0x55 in hex respectively.
-print(buff)
+buff = ctypes.create_string_buffer(85)  
+params = [['switch',1],['LRL',60],['URL',0],['MSR',0],['FAVD',0],['AtrAmp',4.5],['VentAmp',0],['AtrPW',10],['VentPW',0],['AtrSense',0],['VentSense',0],['VRP',0],['ARP',0],['PVARP',0],['RateSmooth',0],['ActivityThresh',0],['ReactTime',0],["RespFact",0],['RecoveryTime',0]] # list to be sent
+                        
+struct.pack_into('<BBBHHHHddddHHHHHHdHHH',buff,0,22,85,params[0][1],params[1][1],params[2][1],params[3][1],params[4][1],params[5][1],params[6][1],params[7][1],params[8][1],params[9][1],params[10][1],params[11][1],params[12][1],params[13][1],params[14][1],params[15][1],params[16][1],params[17][1],params[18][1])# < means Little Endian - uint8,uint8,uint16,uint16,double
+                                                    # parameter 3. can be either 34 -> echo, or 85 -> set. Which is 0x22 and 0x55 in hex respectively.
+# print(buff)
 vals1 = ser.write(buff)  #Write the bytes to the global serial port variable
 print("size of the package we are sending: ", vals1)
-print("calcsize",struct.calcsize("<BBHHd"))
+# print("calcsize",struct.calcsize("<BBHHd"))
 
 time.sleep(1) #1 sec delay
 
 count=0
-
+vals2 = [0]
 while True:  ## Should turn this infinite loop into a function call that will display the Egram
     struct.pack_into('<BB',buff,0,22,34)
     ser.write(buff)
-    vals2 = ser.read(14) #read's argument is the size of the package we are sending/recieving
-    print("reading... ", vals2)  #When you are sending bytes in '85' mode, this will not return anything. When you are sending bytes in '34' mode it will echo your parameters once unless you loop it somehow.
+    # print("Serial Port Info:", ser) 
+ 
+    vals3 = ser.read(85) #read's argument is the size of the package we are sending/recieving
+    vals2 = struct.unpack('<BBBHHHHddddHHHHHHdHHHdd',buff) 
+    # print("reading... ", vals2)  #When you are sending bytes in '85' mode, this will not return anything. When you are sending bytes in '34' mode it will echo your parameters once unless you loop it somehow.
+    # ser.write(buff)   
+
     
+    print(vals3)
+    print(vals2)
+    # print("Serial Port Info:", ser) 
 
 
-    print('unpack buff (in decimal): ', struct.unpack('<BBHHd',buff))
+    # print('unpack buff (in decimal): ', struct.unpack('<BBHHd',buff))
     count += 1
     if count==5:
         break
 
 ser.close()
 
-print("Serial Port Info:", ser) 
+# print("Serial Port Info:", ser) 
